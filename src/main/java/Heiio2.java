39f4a10b3e1aea9bc148bf4366c7af618ab2dee6
ykyk010 <aa01092122338@gmail.com>
2025-03-17 18:10:24 +0900

    변경

---
+++ src/main/java/Heiio2.java
@@ -0,0 +1,6 @@
+public class Heiio2 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+    }
+}
---
+++ src/main/java/Hello3.java
@@ -0,0 +1,11 @@
+public class Hello3 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+
+        int c = a + b;
+    }
+
+
+
+}
