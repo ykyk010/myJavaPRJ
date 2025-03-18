6e17cb2fc8f52763c2f91de3770ced067a233e44
ykyk010 <aa01092122338@gmail.com>
2025-03-18 17:15:34 +0900

    변경

---
+++ src/main/java/Hello7.java
@@ -0,0 +1,13 @@
+public class Hello7 {
+    public static void main(String[] args) {
+        String str1 = "난생처음";
+        String str2 = "자바";
+
+        System.out.println(str1);
+        System.out.println(str2);
+
+        String resuit =  str1 + str2;
+
+        System.out.println(resuit);
+    }
+}
