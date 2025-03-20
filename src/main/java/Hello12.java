25b6688270c6353f6d12adf6ed228f2109c131a8
ykyk010 <aa01092122338@gmail.com>
2025-03-20 19:58:13 +0900

    변경

---
+++ src/main/java/Hello12.java
@@ -0,0 +1,8 @@
+public class Hello12 {
+    public static void main(String[] args) {
+        int a = 38;
+        int b = 262;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
---
+++ src/main/java/Hello13.java
@@ -0,0 +1,10 @@
+public class Hello13 {
+    public static void main(String[] args) {
+        int a = 99;
+        int b = 1;
+
+        int c = a + b;
+        System.out.println(a + "+" + b + "=" + c);
+
+    }
+}
