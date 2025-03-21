dec2714e1bdce0e5a7bbcf827decaea157796b15
ykyk010 <aa01092122338@gmail.com>
2025-03-21 11:21:33 +0900

    변경

---
+++ src/main/java/Hello14.java
@@ -0,0 +1,13 @@
+public class Hello14 {
+    public static void main(String[] args) {
+        int score = 85;
+
+        if (score >= 90) {
+            System.out.println("A 학점");
+        } else if (score >= 80) {
+            System.out.println("B 학점");
+        } else {
+            System.out.println("C 학점");
+        }
+    }
+}
---
+++ src/main/java/Hello15.java
@@ -0,0 +1,14 @@
+public class Hello15 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("반복 횟수: " + i);
+        }
+
+        int j = 1;
+        while (j <= 5) {
+            System.out.println("while 반복 횟수: " + j);
+            j++;
+        }
+    }
+}
