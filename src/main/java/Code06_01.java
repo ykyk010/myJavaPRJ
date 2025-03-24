10625513207bcc498ef37e0be85175ae3546263a
ykyk010 <aa01092122338@gmail.com>
2025-03-24 19:48:07 +0900

    변경

---
+++ src/main/java/Code06_01.java
@@ -0,0 +1,12 @@
+public class Code06_01 {
+    public static void main(String[] args) {
+        int num1, num2, result;
+
+        num1 = 100;
+        num2 = 50;
+
+        result = num1 + num2 ;
+        System.out.println(num1 + "+" + num2 + "=" + result);
+
+    }
+}
---
+++ src/main/java/Code06_02.java
@@ -0,0 +1,11 @@
+public class Code06_02 {
+    public static void main(String[] args) {
+        int num1, num2, result;
+
+        num1 = 100;
+        num2 = 50;
+
+        result = num1 / num2;
+        System.out.println(num1 + "/" + num2 + "=" + result);
+    }
+}
