d924513fbd78ae6d193bd4ebc79eee42e09b035b
ykyk010 <aa01092122338@gmail.com>
2025-03-26 18:48:52 +0900

    변경

---
+++ src/main/java/Exam02.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exam02 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        double result = (double)  a / b;
+
+        System.out.println(a + "/" + b + "=" + result);
+
+        s.close();
+    }
+}
