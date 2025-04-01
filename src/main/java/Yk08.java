95d62d242a80feb1e409d09433c82869c7d6fe09
ykyk010 <aa01092122338@gmail.com>
2025-04-01 09:46:26 +0900

    변경

---
+++ src/main/java/Yk08.java
@@ -0,0 +1,9 @@
+public class Yk08 {
+    public static void main(String[] args) {
+        String var1 = "작은따옴표는 \' 모양입니다.";
+        String var2 = "큰따옴표는 \" 모양입니다.";
+
+        System.out.println(var1);
+        System.out.println(var2);
+    }
+}
