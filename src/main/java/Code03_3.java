6ab2c0112f46c295f60718e99f62744efdd438e8
ykyk010 <aa01092122338@gmail.com>
2025-04-09 15:28:03 +0900

    변경

---
+++ src/main/java/Code03_3.java
@@ -0,0 +1,8 @@
+public class Code03_3 {
+    public static void main(String[] args) {
+        int a = 3, b = 4, c = 5;
+        System.out.println(a + b - c);
+        System.out.println(a - c + b);
+        System.out.println(-c + a + b);
+    }
+}
