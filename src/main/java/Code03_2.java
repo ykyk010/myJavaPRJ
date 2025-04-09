c0b3a86e0b3203656f22fc631a89f3fa200be046
ykyk010 <aa01092122338@gmail.com>
2025-04-09 15:28:51 +0900

    변경

---
+++ src/main/java/Code03_2.java
@@ -0,0 +1,19 @@
+public class Code03_2 {
+    public static void main(String[] args) {
+        int num1, num2, result;
+        num1 = 800;
+        num2 = 60;
+
+        result = num1 + num2;
+        System.out.println(num1 + " + " + num2 + "=" + result);
+
+        result = num1 - num2;
+        System.out.println(num1 + " - " + num2 + "=" + result);
+
+        result = num1 * num2;
+        System.out.println(num1 + " * " + num2 + "=" + result);
+
+        result = num1 / num2;
+        System.out.println(num1 + " / " + num2 + "=" + result);
+    }
+}
