fb11562a29c1bc267f8b58785700a47de3f1a90b
ykyk010 <aa01092122338@gmail.com>
2025-04-10 19:20:55 +0900

    변경

---
+++ src/main/java/Yk24.java
@@ -0,0 +1,10 @@
+public class Yk24 {
+    public static void main(String[] args) {
+        int num = 99;
+
+        if (num < 100) {
+            System.out.println("100보다 ");
+            System.out.println("작습니다.");
+        }
+    }
+}
