411376c0e99cf637d882fe240c88387cab31734f
ykyk010 <aa01092122338@gmail.com>
2025-04-16 13:01:56 +0900

    변경

---
+++ src/main/java/Code02_09.java
@@ -0,0 +1,18 @@
+public class Code02_09 {
+    public static void main(String[] args) {
+
+        int num1, num2, result;
+        num1 = 100;
+        num2 = 60;
+
+        float res,  res2;
+
+        result = num1 / num2;
+        res = num1 / num2;
+        res2 = (float) num1 / num2;
+
+        System.out.println(res + " / " + res2 + " = " + result);
+        System.out.println(res + " / " + res2 + " = " + res);
+        System.out.println(res + " / " + res2 + " = " + res2);
+    }
+}
