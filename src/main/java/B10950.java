526319a71faaa37b0a7770b66f6bb6825a457eca
ykyk010 <aa01092122338@gmail.com>
2025-04-16 13:01:09 +0900

    변경

---
+++ src/main/java/B10950.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class B10950 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int T = s.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+
+        }
+        System.out.println("a +");
+
+
+
+
+        s.close();
+
+    }
+}
