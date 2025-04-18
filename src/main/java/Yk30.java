347541695c08ce392537073cddd21371d2c2157e
ykyk010 <aa01092122338@gmail.com>
2025-04-18 20:27:47 +0900

    변경

---
+++ src/main/java/Yk30.java
@@ -0,0 +1,7 @@
+public class Yk30 {
+    public static void main(String[] args) {
+        for (int i=0 ; i<3 ; i++) {
+            System.out.println(i + ":난생처음 자바는 재밌습니다. ^^");
+        }
+    }
+}
