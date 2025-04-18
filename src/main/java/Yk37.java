a35fedb16f67b1d98a7c1c390b8fd4f143477421
ykyk010 <aa01092122338@gmail.com>
2025-04-18 20:28:29 +0900

    변경

---
+++ src/main/java/Yk37.java
@@ -0,0 +1,14 @@
+public class Yk37 {
+    public static void main(String[] args) {
+
+        int result = 0;
+
+        for(int i = 1; i <=100;i++) {
+            if(i%2 ==0){
+                result += i;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
